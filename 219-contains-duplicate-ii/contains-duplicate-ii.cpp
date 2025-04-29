fec6d1e2ead45213e08366e2930f13489df559b7
Swetha <sricsswetha@gmail.com>
2025-04-29 19:59:38 +0530

    Time: 71 ms (48.67%) | Memory: 98.6 MB (32.58%) - LeetSync

---
+++ 219-contains-duplicate-ii/contains-duplicate-ii.cpp
@@ -0,0 +1,16 @@
+class Solution {
+public:
+    bool containsNearbyDuplicate(vector<int>& nums, int k) {
+        unordered_map<int, int> valueToIndexMap;
+
+        for (int i = 0; i < nums.size(); ++i) {
+            if (valueToIndexMap.count(nums[i])) {
+                if (i - valueToIndexMap[nums[i]] <= k) {
+                    return true;
+                }
+            }
+            valueToIndexMap[nums[i]] = i;
+        }
+        return false;
+    }
+};
